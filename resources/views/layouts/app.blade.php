7dcb59c6504dea17681cbe88a312ecc244600d34
puru9860 <uttam9808202718@gmail.com>
2021-03-04 19:22:36 +0545

    authentication added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset(css/app.css)}} ">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }} ">
     <title>Posts</title>
 </head>
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between">
+
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-5">
         <ul class="flex items-center">
             <li>
-                <a href="">Home</a>
+                <a href="{{route('home')}} " class="p-3">Home</a>
             </li>
             <li>
-                <a href="">Dashboard</a>
+                <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
             </li>
             <li>
-                <a href="">Post</a>
+                <a href="{{route('post')}}" class="p-3">Post</a>
             </li>
+        </ul>
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">{{auth()->user()->name}} </a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                        @csrf
+                        <button  >Logout</button>
+                    </form>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="{{ route('login') }} " class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }} " class="p-3">Register</a>
+                </li>
+            @endguest
+
+
+
         </ul>
 
     </nav>
-@yield('content')
+    @yield('content')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login',[LoginController::class,'store']);
+
+Route::post('/logout',[LogoutController::class,'index'])->name('logout');
+
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/post', function () {
     return view('post.index');
-});
+})->name('post');
